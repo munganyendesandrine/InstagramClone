338115ed967ce6355d69e6766b53841f5b89b04d
munganyendesandrine <mungasandwingsd@gmail.com>
2019-03-25 14:51:28 +0200

    registration form

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'instagram_cloneapp.apps.InstagramCloneappConfig',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
