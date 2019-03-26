745183be93845e3070b5d5683050ef0f6d498432
munganyendesandrine <mungasandwingsd@gmail.com>
2019-03-26 15:27:45 +0200

    css stylings updated

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -123,7 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 
 LOGIN_REDIRECT_URL = 'myprofile'
 
